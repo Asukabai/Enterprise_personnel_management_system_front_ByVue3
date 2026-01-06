8983e82b4f35c7309b213e5df30fbce70edd47a8
Asukabai <3293775641@qq.com>
2026-01-06 10:04:32 +0800

    引入模块组件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <router-view />
+  <div id="app">
+    <BasicLayout />
+  </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-}
+<script setup >
+import BasicLayout from "@/layouts/BasicLayout.vue";
 </script>
-
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="basicLayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a >
+          企业人员管理系统
+        </a>
+        <a href="https://github.com/Asukabai" target="_blank">
+          Github
+        </a>
+        <a href="https://asukabai.blog.csdn.net" target="_blank">
+          博客
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<script setup>
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
+
+<style scoped>
+#basicLayout {
+}
+#basicLayout .content {
+  background: linear-gradient(to right, #fefefe, #fff);
+  margin-bottom: 28px;
+  padding: 20px;
+}
+
+#basicLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
